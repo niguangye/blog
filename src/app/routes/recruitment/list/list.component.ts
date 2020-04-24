34a06fa863d1e9ed463630aeadab581369d29900
倪广野 <1542850280@qq.com>
2020-04-24 17:17:18 +0800

    [Content] 求职页增加字节跳动项

--- src/app/routes/recruitment/list/list.component.ts
+++ src/app/routes/recruitment/list/list.component.ts
@@ -18,10 +18,17 @@ export class RecruitmentListComponent implements OnInit {
   ngOnInit() {
     this.loading = true;
     this.list = [
-      {'title':'快手', 'avatar':'assets/logos/kuaishou.jpg',
+      {'title':'快手',
+        'avatar':'assets/logos/kuaishou.jpg',
+        'href':'https://zhaopin.kuaishou.cn/recruit/e/#/official/social/',
         'description': '快手是记录和分享大家生活的平台，每天数百万的原创新鲜视频\n在这里，发现真实有趣的世界。'
       },
-
+      {
+        'title':'字节跳动',
+        'avatar':'assets/logos/zijie.png',
+        'href':'https://job.bytedance.com/society/position',
+        'description': `和优秀的人，做有挑战的事\n字节跳动拥有较为完善的职业发展体系，充分贴近年轻员工追求自我发展的定位，为员工提供具有针对性和差异化的培训项目`
+      }
     ];
     this.loading = false;
   }
